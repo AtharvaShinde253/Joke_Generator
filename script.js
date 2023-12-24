d90ffb1a40caca4ab81ae48bf3183b215ddb674f
Atharva253 <atharvashinde253@gmail.com>
2023-12-24 22:45:59 +0530

    Created a new API key

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btnEl = document.getElementById("btn");
 const jokeEl = document.getElementById("joke");
 
-const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
+const apiKey = "pQ+QJFPyfTe+9idJQMjOgw==BqHvuhfDRSlOsbsu";
 
 const options = {
   method: "GET",
